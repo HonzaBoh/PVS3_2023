4ec6e87efa39036377ea9b4dd8e16d0f7f77b022
bohac <honza.boh00@gmail.com>
2023-12-14 08:24:00 +0100

    Added line to loops

--- src/oop/GodforsakenAbomination.java
+++ src/oop/GodforsakenAbomination.java
@@ -3,18 +3,16 @@ package oop;
 public class GodforsakenAbomination {
     
     public static void main(String[] args) {
-
-
-
         outer:
         for (int i = 0; i < 1100; i++) {
             System.out.println("Counted i");
             inner:
             for (int j = 0; j < 1000000; j++) {
-                if (j == 1000){
+                if (j == 100){
                     break outer;
                 }
             }
         }
+        System.out.println("Line added");
     }
 }
