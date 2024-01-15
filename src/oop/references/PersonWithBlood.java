668c2d25f04e3cc24910315fb9ecaca0ac19043c
bohac <honza.boh00@gmail.com>
2024-01-15 13:38:13 +0100

    Setup, BloodType

--- src/oop/references/PersonWithBlood.java
+++ src/oop/references/PersonWithBlood.java
@@ -17,11 +17,16 @@ public class PersonWithBlood {
         this.blood = blood;
     }
 
-    void loadData(ArrayList<PersonWithBlood> persons){
+    @Override
+    public String toString() {
+        return name + "(" + blood.toString() + ")";
+    }
+
+    static void loadData(ArrayList<PersonWithBlood> persons) {
         DataImport di = new DataImport("darciKrve.txt");
         String line;
         String[] params;
-        while (di.hasNext()){
+        while (di.hasNext()) {
             line = di.readLine();
             params = line.split(",");
             persons.add(new PersonWithBlood(params[0], Integer.parseInt(params[1]),
@@ -32,23 +37,49 @@ public class PersonWithBlood {
         di.finishImport();
     }
 
+    boolean compatibleToGive(PersonWithBlood toGive) {
+//        this.blood, toGive.blood
+        return true;
+    }
+
+    static void mockDonations(ArrayList<PersonWithBlood> persons) {
+        PersonWithBlood first;
+        PersonWithBlood second;
+        for (int i = 0; i < 1000; i++) {
+            first = persons.get((int) (Math.random() * persons.size()));
+            second = persons.get((int) (Math.random() * persons.size()));
+            if (first.compatibleToGive(second)) {
+                System.out.println("Osoba: " + first + " uspesne darovano: " + second);
+            }
+            else {
+                System.out.println("Nekompatibilni typy:");
+                System.out.println(first);
+                System.out.println(second);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         ArrayList<PersonWithBlood> persons = new ArrayList<>();
+        loadData(persons);
+        mockDonations(persons);
     }
-} enum BloodType {
-   O_PLUS("O+"),
-   O_MINUS("O-"),
-   A_MINUS("A-"),
-   A_PLUS("A+"),
-   AB_MINUS("AB-"),
-   AB_PLUS("AB+"),
-   B_PLUS("B+"),
-   B_MINUS("B-");
+}
+
+enum BloodType {
+    O_PLUS("O+"),
+    O_MINUS("O-"),
+    A_MINUS("A-"),
+    A_PLUS("A+"),
+    AB_MINUS("AB-"),
+    AB_PLUS("AB+"),
+    B_PLUS("B+"),
+    B_MINUS("B-");
 
     BloodType(String s) {
     }
 
-    static BloodType eval(String toEval){
+    static BloodType eval(String toEval) {
         return switch (toEval) {
             case "O+" -> O_PLUS;
             case "O-" -> O_MINUS;
