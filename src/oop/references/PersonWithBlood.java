3ef03d1ffa8c6f5dbc422e80dae96753f61fda2a
bohac <honza.boh00@gmail.com>
2024-01-15 14:00:43 +0100

    Setup, BloodType

--- src/oop/references/PersonWithBlood.java
+++ src/oop/references/PersonWithBlood.java
@@ -38,8 +38,7 @@ public class PersonWithBlood {
     }
 
     boolean compatibleToGive(PersonWithBlood toGive) {
-//        this.blood, toGive.blood
-        return true;
+        return BloodType.BLOOD_COMPATIBILITY[this.blood.position][toGive.blood.position];
     }
 
     static void mockDonations(ArrayList<PersonWithBlood> persons) {
@@ -67,17 +66,33 @@ public class PersonWithBlood {
 }
 
 enum BloodType {
-    O_PLUS("O+"),
-    O_MINUS("O-"),
-    A_MINUS("A-"),
-    A_PLUS("A+"),
-    AB_MINUS("AB-"),
-    AB_PLUS("AB+"),
-    B_PLUS("B+"),
-    B_MINUS("B-");
+    A_PLUS("A+",0),
+    A_MINUS("A-",1),
+    B_PLUS("B+",2),
+    B_MINUS("B-",3),
+    O_PLUS("O+",4),
+    O_MINUS("O-",5),
+    AB_PLUS("AB+",6),
+    AB_MINUS("AB-",7);
 
-    BloodType(String s) {
+    BloodType(String s, int position) {
+        this.position = position;
     }
+
+    public final int position;
+    public static final boolean[][] BLOOD_COMPATIBILITY = {
+//          A+    A-    B+      B-    0+    0-    AB+    AB-
+            { true, true, false, false, true, true, false, false },  // A+
+            { false, true, false, false, false, true, false, false}, // A-
+            { false, false, true, true, true, true, false, false},   // B+
+            { false, false, false, true, false, true, false, false}, // B-
+            { false, false, false, false, true, true, false, false}, // 0+
+            { false, false, false, false, false, true, false, false},// 0-
+            { true, true, true, true, true, true, true, true},       // AB+
+            { false, true, false, true, false, true, false, true}    // AB-
+
+    };
+
 
     static BloodType eval(String toEval) {
         return switch (toEval) {
