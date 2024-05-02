8fc30f7079a79b3fde0f777765a6548d9dd5b928
bohac <honza.boh00@gmail.com>
2024-05-02 08:10:27 +0200

    Streams, maps setup

--- src/oop/collections/mapping/MovieMapping.java
+++ src/oop/collections/mapping/MovieMapping.java
@@ -1,30 +1,28 @@
 package oop.collections.mapping;
 
-public class MoveMapping {
+public class MovieMapping {
     String name;
     int yearOfRelease;
     String genre;
     double rating;
-    int duration;//tbd
-    String director;
+//    int duration;//tbd
+//    String director;
 
-    public MoveMapping(String name, int yearOfRelease, String genre, double rating, int duration, String director) {
+    public MovieMapping(String name, int yearOfRelease, String genre) {
         this.name = name;
         this.yearOfRelease = yearOfRelease;
         this.genre = genre;
-        this.rating = rating;
-        this.duration = duration;
-        this.director = director;
+        this.rating = 0;
     }
 
     @Override
     public String toString() {
         return "===================\n"
                  + "Name: " + name + "\n"
-                 + "Written by: " + director + "\n"
+//                 + "Written by: " + director + "\n"
                  + "Genre: " + genre + "\n"
                  + "Released in: " + yearOfRelease + "\n"
-                 + "Runtime: " + duration + "\n" /*todo: mm:ss  format, dle datasetu */
+//                 + "Runtime: " + duration + "\n"
                  + "Rated: " + rating + "10\n"
                 + "===================\n";
     }
